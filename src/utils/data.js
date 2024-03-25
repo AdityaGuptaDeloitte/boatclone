d3a73ff80b106609236c91f8819a130e693790f9
AdityaGuptaDeloitte <163412608+AdityaGuptaDeloitte@users.noreply.github.com>
2024-03-25 12:58:09 +0530

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,46 @@
+const categoriesArray = [ 
+
+    {image: `https://shorturl.at/eozAD`, category: "Airdopes", link: "/Products?Category=Airdopes"}, 
+    
+    {image: `https://m.media-amazon.com/images/I/51PyI5qRSwS._SX522_.jpg`, category: "Earphones", link: "/Products?Category=Earphone"}, 
+    
+    {image: `https://m.media-amazon.com/images/I/71to+uN06pL._SX679_.jpg`,category: "Smart Watches", link: "/Products?Category=Smartwatch" }, 
+    
+     {image: `https://m.media-amazon.com/images/I/61PapoEtB+L._SL1500_.jpg`, category: "SoundBars", link: "/Products?Category=Soundbar"}, 
+    
+     {image: `https://m.media-amazon.com/images/I/615Mt56oMuL._SX522_.jpg`, category: "Wireless Speakers", link: '/Products?Category=Speaker'}, 
+    
+]
+export default categoriesArray;
+
+
+export const category_videos = [
+    {link:'https://www.boat-lifestyle.com/cdn/shop/files/quinn_RUxbhR7CvjkNtlFUDxgw9.mp4',href:"?Category=Smartwatch", title:"Smartwatch"},
+    {link:'https://www.boat-lifestyle.com/cdn/shop/files/quinn_Bejc8URjU1NSXdhabLCmD.mp4',href:"?Category=Airdopes", title:"True Wireless Earbuds"},
+    {link:'https://www.boat-lifestyle.com/cdn/shop/files/quinn_OyJHanx4QSdUN3OVGTO7C.mp4',href:"?Category=Earphone", title:"Neckbands"},
+    {link:'https://www.boat-lifestyle.com/cdn/shop/files/quinn_CpsRIdJWtpXyFN3enwbXd.mp4',href:"?Category=Soundbar", title:"Headphones"}
+]
+
+export const review_videos = [
+    {
+        link: 'https://www.boat-lifestyle.com/cdn/shop/files/quinn_wlacs5m0tq7rv91yh1gv9rv7.mp4',
+        title: 'Airdopes 71',
+        price: '999.00',
+    },
+    {
+        link: 'https://www.boat-lifestyle.com/cdn/shop/files/quinn_ymyi7vf3rgm754wocfsjdcuy.mp4',
+        title: 'Airdopes 161 Pro',
+        price: '1099.00',
+    },
+    {
+        link: 'https://www.boat-lifestyle.com/cdn/shop/files/quinn_i6tbapxcxwu1pim6d8tal35d.mp4',
+        title: 'Nirvana Ion ANC',
+        price: '2799.00',
+    },
+    {
+        link: 'https://www.boat-lifestyle.com/cdn/shop/files/quinn_nd03f9bf2bovwzyn8n9vuejb.mp4',
+        title: 'Rockerz 245 V2 Pro',
+        price: '999.00',
+    },
+]
+
